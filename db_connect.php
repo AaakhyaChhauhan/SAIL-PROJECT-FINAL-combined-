9ba5d9de819cd94e132e96bc6c205ad84cb86701
Aakhya Chhauhan <142525613+AaakhyaChhauhan@users.noreply.github.com>
2025-07-24 18:01:01 +0530

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -1,5 +1,5 @@
 <?php
-$conn = new mysqli("sql12.freesqldatabase.com", "sql12791553", "B9Sva3lRQs", "sql12791553", 3306);
+$conn = new mysqli(YOUR DATABASE DETAILS);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
